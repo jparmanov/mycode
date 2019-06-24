890c320eefe41f6d26cdcb6064e33d84b3999616
Jasur <jasur.parmanov@hughes.com>
2019-06-24 14:01:02 -0400

    Update

---
+++ if-test2/iftest2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#ipchk='192.168.0.1'
+ipchk=input('Apply an IP address: ')
+
+
+if ipchk=='192.168.70.1':
+    #print ('Looks like the IP address was set: '+ipchk)
+    print ('Looks like the IP address of the Gateway was set: '+ipchk+' This is not recommended')
+elif ipchk:
+    print ('Looks like the IP address was set: '+ipchk)
+else:
+    print ('You did not provide input')
+
