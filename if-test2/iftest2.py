6e4875162f6a7f5c1627789c888f992fe6e9635c
Jasur <jasur.parmanov@hughes.com>
2019-06-24 14:15:44 -0400

    Update2

--- if-test2/iftest2.py
+++ if-test2/iftest2.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 
-#ipchk='192.168.0.1'
-ipchk=input('Apply an IP address: ')
+import ipaddress
+
+def main():
+    #ipchk='192.168.0.1'
+    ipchk=input('Apply an IP address: ')
+
+    try:
+
+        ipaddress.ip_address(ipchk)
+
+        if ipchk=='192.168.70.1':
+            #print ('Looks like the IP address was set: '+ipchk)
+            print ('Looks like the IP address of the Gateway was set: '+ipchk+' This is not recommended')
+        else:
+            print ('Looks like the IP address was set: '+ipchk)
+        #else:
+        #    print ('You did not provide input')
+    except:
+        print("Invalid IP address")
 
 
-if ipchk=='192.168.70.1':
-    #print ('Looks like the IP address was set: '+ipchk)
-    print ('Looks like the IP address of the Gateway was set: '+ipchk+' This is not recommended')
-elif ipchk:
-    print ('Looks like the IP address was set: '+ipchk)
-else:
-    print ('You did not provide input')
-
+main()
